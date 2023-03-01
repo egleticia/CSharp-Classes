b218a5ca016eb79874aa59d559a5fcce70d2a4d0
egleticia <leticiaegle@yahoo.com.br>
2023-02-28 21:49:58 -0300

    Exerciício Prático

---
+++ Polimorfismo/polimorfismo-exercicio.app/polimorfismo-exercicio/Entities/OutsourceEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorfismo_exercicio.Entities
+{
+    public class OutsourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+
+        public OutsourceEmployee()
+        { 
+        }
+
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base (name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+        }
+
+    }
+}
