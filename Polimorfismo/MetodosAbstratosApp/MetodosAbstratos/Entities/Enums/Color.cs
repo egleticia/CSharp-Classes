5776f91952adb05eb137820ff7d7d851c7b7b22d
egleticia <leticiaegle@yahoo.com.br>
2023-03-03 23:32:58 -0300

    Exercício de Fixação - Métodos Abstratos - Parte I

---
+++ Polimorfismo/MetodosAbstratosApp/MetodosAbstratos/Entities/Enums/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAbstratos.Entities.Enums
+{
+    enum Color : int
+    {
+        Black = 1,
+        Blue = 2,
+        Red = 3
+    }
+}
