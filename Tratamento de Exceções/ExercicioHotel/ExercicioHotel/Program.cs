cbeb6927c0162623918cf1f46498271488d00577
egleticia <leticiaegle@yahoo.com.br>
2023-03-08 22:01:51 -0300

    Exercicio - Tratamento de Exceções - Parte I

---
+++ Tratamento de Exceções/ExercicioHotel/ExercicioHotel/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioHotel.Entities;
+
+Console.WriteLine("Room number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+if (checkOut <= checkIn)
+{
+    Console.WriteLine("Error in reservation: Check-out date must be after check-in date.");
+}
+
+else
+{
+    Reservation reservation = new Reservation(number, checkIn, checkOut);
+    Console.WriteLine("Reservation: " + reservation);
+}
+
+
+
+
