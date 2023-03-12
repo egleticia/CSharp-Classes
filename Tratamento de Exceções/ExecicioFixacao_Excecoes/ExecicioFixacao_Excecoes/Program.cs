e7dabb798a9a8ca91f00dc48b97712a009d13510
egleticia <leticiaegle@yahoo.com.br>
2023-03-11 22:10:21 -0300

    Exercicio de Fixacao (Tratamento de Excecoes) Parte I

---
+++ Tratamento de Exceções/ExecicioFixacao_Excecoes/ExecicioFixacao_Excecoes/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+Console.WriteLine("Enter account data: ");
+
+Console.WriteLine("Number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Holder: ");
+string holder = Console.ReadLine();
+
+Console.WriteLine("Initial balance: ");
+double balance = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Withdraw limit: ");
+double withdrawLimit = double.Parse(Console.ReadLine());
+
