f22974b9701c5dae9f716869bcbc2bc05bf90ba8
egleticia <leticiaegle@yahoo.com.br>
2023-03-13 22:53:21 -0300

    Trabalhando com arquivos - Aula I

---
+++ Trabalhando com Arquivos/Files/Files/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+string sourcePath = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\File1.txt";
+string targetPath = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\File2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(targetPath);
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred.");
+    Console.WriteLine(e.Message);
+}
