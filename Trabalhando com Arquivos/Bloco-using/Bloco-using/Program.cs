ae9e210b9dcb7e0b02669d681af9bf47b0fc96d3
egleticia <leticiaegle@yahoo.com.br>
2023-03-15 22:00:16 -0300

    Trabalhando com arquivos - Aula III

---
+++ Trabalhando com Arquivos/Bloco-using/Bloco-using/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string path = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\File1.txt";
+
+
+// executa o que está no bloco e após executar é automaticamente fechado
+
+try
+{
+
+
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+
+}
+
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred.");
+    Console.WriteLine(e.Message);
+}
