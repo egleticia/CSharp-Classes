820e1b286c9037e5169530714d581b8efb4c9dfd
egleticia <leticiaegle@yahoo.com.br>
2023-03-16 22:15:50 -0300

    Trabalhando com arquivos - Aula IV

---
+++ Trabalhando com Arquivos/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string sourcePath = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\File1.txt";
+string targetPath = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\File2.txt";
+
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using (StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred.");
+    Console.WriteLine(e.Message);
+}
