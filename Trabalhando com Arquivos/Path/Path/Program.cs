2e019ebe3a4d1bfc505f957b6af1b2ad54c8abee
egleticia <leticiaegle@yahoo.com.br>
2023-03-18 13:16:55 -0300

    Trabalhando com arquivos - Aula VI

---
+++ Trabalhando com Arquivos/Path/Path/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+string path = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\myfolder\File1.txt";
+
+Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+Console.WriteLine("GetTempPath: " + Path.GetTempPath());
