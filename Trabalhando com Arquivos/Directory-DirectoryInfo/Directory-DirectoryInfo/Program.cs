30f98785a96bd9f25586feba852106f0ecff54d0
egleticia <leticiaegle@yahoo.com.br>
2023-03-17 22:43:10 -0300

    Trabalhando com arquivos - Aula V

---
+++ Trabalhando com Arquivos/Directory-DirectoryInfo/Directory-DirectoryInfo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+string path = @"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\myfolder";
+
+
+try
+{
+   var folders =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+    Console.WriteLine("FOLDERS: ");
+
+    foreach (string s in folders)
+    {
+        Console.WriteLine(s);
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error eccurred.");
+    Console.WriteLine(e.Message);
+}
+
+Directory.CreateDirectory(@"c:\Projetos Studio\CSharp-Classes\Trabalhando com Arquivos\myfolder\newfolder");
+//Directory.CreateDirectory(path + @"\newfolder");
+
+try
+{
+    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+
+    Console.WriteLine("FILES: ");
+
+    foreach (string s in files)
+    {
+        Console.WriteLine(s);
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error eccurred.");
+    Console.WriteLine(e.Message);
+}
