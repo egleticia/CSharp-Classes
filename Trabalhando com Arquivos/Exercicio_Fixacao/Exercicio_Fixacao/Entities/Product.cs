93a0d81b4b1598a577849d493710643be14573ec
egleticia <leticiaegle@yahoo.com.br>
2023-03-19 21:55:04 -0300

    Trabalhando com Arquivos - Exercício Fixacao Parte I

---
+++ Trabalhando com Arquivos/Exercicio_Fixacao/Exercicio_Fixacao/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Fixacao.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+
+        public Product()
+        {
+
+        }
+        public Product (string name, double price, int quantity)
+        {
+
+        }
+
+    }
+}
