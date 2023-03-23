3800b5ebf1159e361459adf820a88208bdef5df6
egleticia <leticiaegle@yahoo.com.br>
2023-03-23 18:42:18 -0300

    Interface I

---
+++ Interface/AluguelDeCarrosApp/AluguelDeCarros/Entities/CarRental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelDeCarros.Entities
+{
+    public class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public CarRental() { }
+
+        public CarRental(DateTime start, DateTime finish)
+        {
+            Start = start;
+            Finish = finish;
+
+        }
+    }
+}
