39bab3b1bed727e771db707ae643281130d5f014
egleticia <leticiaegle@yahoo.com.br>
2023-03-27 23:17:10 -0300

    Interface II

--- Interface/AluguelDeCarrosApp/AluguelDeCarros/Entities/Vehicle.cs
+++ Interface/AluguelDeCarrosApp/AluguelDeCarros/Entities/Vehicle.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AluguelDeCarros.Entities
+﻿namespace AluguelDeCarros.Entities
 {
     public class Vehicle
     {
         public string CarModel { get; set; }
 
-        public Vehicle() { }
-
         public Vehicle(string carModel)
+
         {
             CarModel = carModel;
         }
