2eb22b89c8afefcce8ebac8cf8c016f13aa4f1f3
egleticia <leticiaegle@yahoo.com.br>
2023-03-28 22:42:25 -0300

    Interface III

--- Interface/AluguelDeCarrosApp/AluguelDeCarros/Entities/CarRental.cs
+++ Interface/AluguelDeCarrosApp/AluguelDeCarros/Entities/CarRental.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AluguelDeCarros.Entities
+﻿namespace AluguelDeCarros.Entities
 {
-    public class CarRental
+    class CarRental
     {
         public DateTime Start { get; set; }
         public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
 
         public CarRental() { }
 
-        public CarRental(DateTime start, DateTime finish)
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
         {
             Start = start;
             Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
 
         }
     }
--- Interface/AluguelDeCarrosApp/AluguelDeCarros/Program.cs
+++ Interface/AluguelDeCarrosApp/AluguelDeCarros/Program.cs
@@ -1,2 +1,17 @@
 ﻿
+using System.Globalization;
+using AluguelDeCarros.Entities;
+
+Console.WriteLine("Enter rental data: ");
+Console.WriteLine("Car model: ");
+string model = Console.ReadLine();
+
+Console.WriteLine("Pickup: ");
+DateTime start = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+Console.WriteLine("Return: ");
+DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
 
