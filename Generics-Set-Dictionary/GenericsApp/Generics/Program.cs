770b697c8f685b974f86a8e424242024d210fc0f
egleticia <leticiaegle@yahoo.com.br>
2023-04-04 21:59:55 -0300

    Generics I

---
+++ Generics-Set-Dictionary/GenericsApp/Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using Generics;
+
+
+
+PrintService printService= new PrintService();
+Console.WriteLine("How many Values? ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.addValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
