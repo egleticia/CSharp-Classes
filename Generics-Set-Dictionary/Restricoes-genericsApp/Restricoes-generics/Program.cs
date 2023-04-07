7a09e1d73356ab61a7436d3d6437277c711897eb
egleticia <leticiaegle@yahoo.com.br>
2023-04-07 16:05:38 -0300

    Restricoes Generics I

--- Generics-Set-Dictionary/Restricoes-genericsApp/Restricoes-generics/Program.cs
+++ Generics-Set-Dictionary/Restricoes-genericsApp/Restricoes-generics/Program.cs
@@ -1,2 +1,20 @@
-﻿
+﻿using Restricoes_generics.Services;
 
+
+
+List<int> list = new List<int>();
+
+Console.WriteLine("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    list.Add(x);
+}
+
+CalculationService calculationService = new CalculationService();
+
+int max = calculationService.Max(list);
+Console.WriteLine("Max: ");
+Console.WriteLine(max);
--- Generics-Set-Dictionary/Restricoes-genericsApp/Restricoes-generics/Services/CalculationService.cs
+++ Generics-Set-Dictionary/Restricoes-genericsApp/Restricoes-generics/Services/CalculationService.cs
@@ -3,6 +3,32 @@ namespace Restricoes_generics.Services
 {
     class CalculationService
     {
+        public int Max(List<int> list)
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty.");
+            }
+
+            int max = list[0];
+
+            for(int i = 1; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                {
+                    max= list[i];
+                }
+            }
+
+            return max;
+        }
+        
+        
+        
+        
+        
+      
+
 
     }
 }
