6d7740ba8d5d6ef2c89bd932be4c1087e19eb655
egleticia <leticiaegle@yahoo.com.br>
2023-04-09 23:20:16 -0300

    GetHashCode e Equals

---
+++ Generics-Set-Dictionary/GetHashCode-EqualsApp/GetHashCode-Equals/Program.cs
@@ -0,0 +1,13 @@
+﻿using GetHashCode_Equals.Entities;
+
+
+// foi definido como criterio o email do cliente, assim se os dois emails forem iguais, o retorno será true
+
+Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a == b);
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
