326b182072f771c70cabe59899522fb90895f456
egleticia <leticiaegle@yahoo.com.br>
2023-04-10 21:40:54 -0300

    HashSet e SortedSet I

---
+++ Generics-Set-Dictionary/HashSet-SortedSetApp/HashSet-SortedSet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+//Representa conjunto de elementos: não permite repetições, elementos não possuem posição, acesso, inserção e remoção de elementos são rápidos
+
+
+// instânciando um conjunto
+HashSet<string> set = new HashSet<string>();
+
+set.Add("TV");
+set.Add("Notebook");
+set.Add("Tablet");
+
+
+foreach (string p in set)
+    Console.WriteLine(p);
