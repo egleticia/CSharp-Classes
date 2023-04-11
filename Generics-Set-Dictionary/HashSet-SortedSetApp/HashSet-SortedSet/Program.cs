1f069dd2846e24c410498f0fec78a68362d32980
egleticia <leticiaegle@yahoo.com.br>
2023-04-10 22:00:17 -0300

    HashSet e SortedSet II

--- Generics-Set-Dictionary/HashSet-SortedSetApp/HashSet-SortedSet/Program.cs
+++ Generics-Set-Dictionary/HashSet-SortedSetApp/HashSet-SortedSet/Program.cs
@@ -13,3 +13,45 @@ set.Add("Tablet");
 
 foreach (string p in set)
     Console.WriteLine(p);
+
+Console.WriteLine("-------------------------------------");
+
+// SortedSet mantém os elementos ordenados
+SortedSet<int> a = new SortedSet<int>()
+{
+    10,0,2,4,5,6,8
+};
+
+SortedSet<int> b = new SortedSet<int>()
+{
+    5,6,7,8,9,10
+};
+
+//Union 
+SortedSet<int> c = new SortedSet<int>(a);
+c.UnionWith(b);
+
+PrintCollection(c);
+
+//Intersection 
+SortedSet<int> d = new SortedSet<int>(a);
+d.IntersectWith(b);
+
+PrintCollection(d);
+
+//Difference 
+SortedSet<int> e = new SortedSet<int>(a);
+e.ExceptWith(b);
+
+PrintCollection(e);
+
+
+static void PrintCollection<T>(IEnumerable<T> collection)
+{
+    //foreach funciona em cima de coleções que implementam o IEnumerable
+    foreach (T obj in collection)
+    {
+        Console.Write(obj + " ");
+    }
+    Console.WriteLine();
+}
