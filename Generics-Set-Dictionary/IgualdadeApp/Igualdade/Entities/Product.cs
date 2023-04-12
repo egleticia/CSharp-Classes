55b320942ed9a3aa63e0ecae452b66a468f0395d
egleticia <leticiaegle@yahoo.com.br>
2023-04-11 23:18:13 -0300

    Comparação de igualdade em coleções Hash

---
+++ Generics-Set-Dictionary/IgualdadeApp/Igualdade/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Igualdade.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;   
+            Price = price;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            // Implementando a programção defensiva
+            if(!(obj is Product))
+                return false;
+
+            //realizando um downcasting
+            Product other = (Product)obj;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+
+}
---
+++ Generics-Set-Dictionary/IgualdadeApp/Igualdade/Program.cs
@@ -0,0 +1,20 @@
+﻿using Igualdade.Entities;
+
+HashSet<Product> a = new HashSet<Product>();
+a.Add(new Product("TV", 900.00));
+a.Add(new Product("Notebook", 1200.00));
+
+
+HashSet<Point> b = new HashSet<Point>();
+b.Add(new Point(3,4));
+b.Add(new Point(5,10));
+
+Product prod = new Product("Notebook", 1200.00);
+
+// Retorno "False", embora seja o mesmo conteúdo, pois compara as referências de memória dos objetos, eram dois objetos diferentes, por isso falso;
+// Após a implementação do override de Equals e HashCode o retorno é verdadeiro, pois compara o conteúdo
+Console.WriteLine(a.Contains(prod));
+
+// No caso de Point, mesmo não implementando o HashCode e Equals, o retorno é verdade, pois o tipo é STRUCT, e assim compara por conteúdo e não referência
+Point p = new Point(3,4);
+Console.WriteLine(b.Contains(p));
