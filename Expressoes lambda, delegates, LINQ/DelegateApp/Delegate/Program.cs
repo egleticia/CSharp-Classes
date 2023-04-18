46a5d44da80c34d4b6ce34f5d6050142118669d5
egleticia <leticiaegle@yahoo.com.br>
2023-04-17 22:43:56 -0300

    Delegate

---
+++ Expressoes lambda, delegates, LINQ/DelegateApp/Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using Delegate.Services;
+using System;
+
+delegate double BinaryNumerocOperation(double n1, double n2);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+
+        BinaryNumerocOperation op = CalculationService.Max;
+        BinaryNumerocOperation op1 = new BinaryNumerocOperation(CalculationService.Max);
+        //double result = CalculationService.Sum(a, b);
+
+        double result = op.Invoke(a, b);
+        Console.WriteLine(result);
+    }
+}
+
