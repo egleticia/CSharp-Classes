acfb3acd46040c822058cd26327325c249435ada
egleticia <leticiaegle@yahoo.com.br>
2023-04-18 22:44:24 -0300

    Multicast Delegates

---
+++ Expressoes lambda, delegates, LINQ/Multicast-delegates/Multicast-delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Multicast_delegates.Services;
+using System;
+
+delegate void BinaryNumerocOperation(double n1, double n2);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+
+        BinaryNumerocOperation op = CalculationService.ShowSum;
+        op += CalculationService.ShowMax;
+
+        //op.Invoke(a, b);
+        op (a,b);
+    }
+}
+
+
