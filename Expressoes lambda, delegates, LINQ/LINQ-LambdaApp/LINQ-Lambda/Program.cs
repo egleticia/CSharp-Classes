aacf1e5c8ad665622241c43af9e1a197499b8e49
egleticia <leticiaegle@yahoo.com.br>
2023-04-26 23:44:31 -0300

    LINQ com Lambda - Parte II

--- Expressoes lambda, delegates, LINQ/LINQ-LambdaApp/LINQ-Lambda/Program.cs
+++ Expressoes lambda, delegates, LINQ/LINQ-LambdaApp/LINQ-Lambda/Program.cs
@@ -1,2 +1,37 @@
 ﻿using LINQ_Lambda.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+
+List<Product> products = new List<Product>()
+{
+    new Product(){ Id = 1, Name = "Computer", Price = 1100.0, Category = c2},
+    new Product(){ Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+    new Product(){ Id = 2, Name = "TV", Price = 1700.0, Category = c3 },
+    new Product(){ Id = 2, Name = "Notebook", Price = 1300.0, Category = c2 },
+    new Product(){ Id = 2, Name = "Saw", Price = 80.0, Category = c1 },
+    new Product(){ Id = 2, Name = "Tablet", Price = 700.0, Category = c2 },
+    new Product(){ Id = 2, Name = "Camera", Price = 700.0, Category = c3},
+    new Product(){ Id = 2, Name = "Printer", Price = 350.0, Category = c3 },
+    new Product(){ Id = 2, Name = "MacBook", Price = 1800.0, Category = c2 },
+    new Product(){ Id = 2, Name = "Sound Bar", Price = 700.0, Category = c3},
+    new Product(){ Id = 2, Name = "Level", Price = 70.0, Category = c1 },
+};
+
+var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+
+Print("TIER 1 AND PRICE < 900:", r1);
+
+
+
+static void Print<T>(string message, IEnumerable<T> collection)
+{
+    Console.WriteLine(message);
+    foreach (T item in collection) Console.WriteLine(item);
+
+    Console.WriteLine();
+}
 
