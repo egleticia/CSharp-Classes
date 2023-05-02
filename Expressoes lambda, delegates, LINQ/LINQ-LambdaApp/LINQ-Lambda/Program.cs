363ba0ff242913f9b88a3ccc125f706eb055c0e6
egleticia <leticiaegle@yahoo.com.br>
2023-05-01 21:50:48 -0300

    LINQ com notacao similar à SQL

--- Expressoes lambda, delegates, LINQ/LINQ-LambdaApp/LINQ-Lambda/Program.cs
+++ Expressoes lambda, delegates, LINQ/LINQ-LambdaApp/LINQ-Lambda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LINQ_Lambda.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Windows.Markup;
 
 Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
@@ -25,24 +26,66 @@ List<Product> products = new List<Product>()
 var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
 Print("TIER 1 AND PRICE < 900:", r1);
 
+var s1 =
+    from p in products
+    where p.Category.Tier == 1 && p.Price < 900
+    select p;
+Print("TIER 1 AND PRICE < 900:", s1);
+
 var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
 Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+
+var s2 =
+    from p in products
+    where p.Category.Name == "Tools"
+    select p.Name;
+Print("NAMES OF PRODUCTS FROM TOOLS", s2);
 
 var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new {p.Name, p.Price, CategoryName = p.Category.Name});
 Print("NAMES STARTED WITH C AND ANONYMOUS OBJECT", r3);
 
+var s3 =
+    from p in products
+    where p.Name[0] == 'C'
+    select new
+    {
+        p.Name,
+        p.Price,
+        CategoryName = p.Category.Name,
+    };
+Print("NAMES STARTED WITH C AND ANONYMOUS OBJECT", s3);
+
 var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Category.Name);
 Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+var s4 =
+   from p in products
+   where p.Category.Tier == 1
+   orderby p.Name
+   orderby p.Price
+   select p;
+Print("TIER 1 ORDER BY PRICE THEN BY NAME", s4);
 
 // Paginacao 
 var r5 = r4.Skip(2).Take(4);
 Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+
+var s5 =
+    (from p in s4
+     select p).Skip(2).Take(4);
+Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", s5);
 
 var r6 = products.First();
 Console.WriteLine("First test 1: " + r6);
 
 var r7 = products.Where(p => p.Price > 3000).FirstOrDefault();
 Console.WriteLine("FirstOrDefault test 2: " + r7);
+
+var s7 =
+    (from p in products
+     where p.Price > 3000.0
+     select p).FirstOrDefault();
+Console.WriteLine("FirstOrDefault test 2: " + s7);
 
 Console.WriteLine("---------------------------");
 
@@ -82,6 +125,20 @@ foreach (IGrouping<Category, Product> group in r16)
         Console.WriteLine(p);
     }
     Console.WriteLine(  );
+}
+
+var s16 =
+    from p in products
+    group p by p.Category;
+foreach (IGrouping<Category, Product> group in s16)
+{
+    Console.WriteLine("Category " + group.Key.Name + ": ");
+
+    foreach (Product p in group)
+    {
+        Console.WriteLine(p);
+    }
+    Console.WriteLine();
 }
 
 static void Print<T>(string message, IEnumerable<T> collection)
