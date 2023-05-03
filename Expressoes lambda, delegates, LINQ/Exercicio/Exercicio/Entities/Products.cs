2c8764db8e418b6a2621db8892cd8a88af392c3a
egleticia <leticiaegle@yahoo.com.br>
2023-05-02 23:39:46 -0300

    Exercicio - LINQ

---
+++ Expressoes lambda, delegates, LINQ/Exercicio/Exercicio/Entities/Products.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Exercicio.Entities
+{
+    public class Products
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Expressoes lambda, delegates, LINQ/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Exercicio.Entities;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+
+Console.WriteLine("Enter full file path: ");
+string path = Console.ReadLine();
+
+
+List<Products> products = new List<Products>();
+
+using (StreamReader sr = File.OpenText(path))
+{
+    while (!sr.EndOfStream)
+    {
+        string[] fields = sr.ReadLine().Split(',');
+        string name = fields[0];
+        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+        products.Add(new Products(name, price));
+
+    }
+}
+
+var avg = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+Console.WriteLine("Average Price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+Console.WriteLine(names);
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
